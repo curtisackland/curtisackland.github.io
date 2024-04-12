97a033bbbe00755ee9ecda91750403bb4c68baeb
Curtman <curtisackland@rogers.com>
2024-04-12 13:48:08 -0400

    Updated home page and icon

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/experience">Experience</router-link> |
-    <router-link to="/projects">Projects</router-link> |
-    <router-link to="/resume">Resume</router-link> |
-    <router-link to="/contact">Contact</router-link>
+    <router-link to="/">
+      <h2>C</h2>
+    </router-link>
+    <div class="links">
+      <router-link to="/">About</router-link>&nbsp;
+      <router-link to="/experience">Experience</router-link>&nbsp;
+      <router-link to="/projects">Projects</router-link>&nbsp;
+      <router-link to="/resume">Resume</router-link>&nbsp;
+      <router-link to="/contact">Contact</router-link>
+    </div>
   </nav>
   <router-view />
 </template>
@@ -13,15 +18,39 @@
 nav {
   padding: 30px;
   text-align: center;
+  display: flex;
+  justify-content: space-between;
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: $baseFontColor;
+    text-decoration: none;
+    margin: 0 8px 0 8px;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: $accentColor;
     }
   }
+}
+
+.links {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+}
+
+h2 {
+  color: $accentColor;
+  margin: 0;
+  font-size: 2em;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  border: 2px solid $accentColor;
+  border-radius: 5px;
+  height: 1.5em;
+  aspect-ratio: 1 / 1;
 }
 </style>
 <script setup></script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,43 +1,42 @@
 <template>
   <div class="home-container">
-    <div class="profile-column">
-      <h1>Curtis Ackland</h1>
-      <img
-        alt="Profile Picture"
-        src="../assets/profile_picture.png"
-        class="profile-picture"
-      />
-      <div class="icons-space">
-        <a href="https://github.com/curtisackland" target="_blank">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            width="32"
-            height="32"
-            class="link-icons"
-          >
-            <path
-              d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
-            ></path>
-          </svg>
+    <h4 class="nice-to-meet-you">Nice to meet you, my name is</h4>
+    <h1 class="name-text">Curtis Ackland.</h1>
+    <p class="about-me">
+      I am based in London, Ontario and I am a recent Computer Science graduate
+      of the University of Western Ontario. I am currently seeking opportunities
+      to leverage full stack internship experience and academic knowledge to
+      contribute to high-quality software solutions within a collaborative team
+      environment.
+    </p>
+    <div class="icons-space">
+      <button class="contact-me-button">
+        <a class="mail-to-link" href="mailto: curtisackland@rogers.com">
+          Contact Me
         </a>
-        <a href="https://github.com/curtisackland" target="_blank">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="32"
-            height="32"
-          >
-            <path
-              d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"
-            ></path>
-          </svg>
-        </a>
-      </div>
-    </div>
-    <div>
-      <h2>About Me</h2>
-      section paragraph
+      </button>
+      <a href="https://github.com/curtisackland" target="_blank">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          class="link-icons"
+        >
+          <path
+            d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+          ></path>
+        </svg>
+      </a>
+      <a href="https://www.linkedin.com/in/curtis-ackland/" target="_blank">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          class="link-icons"
+        >
+          <path
+            d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"
+          ></path>
+        </svg>
+      </a>
     </div>
   </div>
 </template>
@@ -48,31 +47,54 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
 .home-container {
-  display: flex;
-  flex-wrap: wrap;
+  white-space: normal;
   justify-content: center;
-}
-
-.profile-column {
-  margin: 8px;
-}
-
-.profile-picture {
-  width: 12em;
-  height: auto;
-  border-radius: 30px;
+  padding: 5% 25% 0 25%;
 }
 
 .icons-space {
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-around;
-  padding: 8px;
+  width: 75%;
+  justify-content: space-between;
+  height: 60px;
 }
 
 .link-icons {
-  fill: white;
+  fill: $baseFontColor;
+  height: 60px;
+  width: auto;
+  padding: 4px 0 4px 0;
+}
+
+.about-me {
+  font-size: 1.2em;
+  line-height: 200%;
+}
+
+.contact-me-button {
+  background-color: rgba(255, 165, 0, 0.05);
+  border-color: $accentColor;
+  color: $accentColor;
+  border-radius: 10px;
+  font-size: 1.2em;
+  padding: 16px;
+}
+
+.mail-to-link {
+  text-decoration: none;
+  color: $accentColor;
+}
+
+.nice-to-meet-you {
+  color: $accentColor;
+  margin: 0;
+}
+
+.name-text {
+  font-size: 4em;
+  white-space: normal;
 }
 </style>
